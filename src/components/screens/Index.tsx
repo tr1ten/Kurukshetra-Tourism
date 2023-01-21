5829ca90ac28836e6e6b8bc6331400dd10ddca47
Sushmit-Bhalothia <sushmitbhalothia@gmail.com>
2023-01-21 16:52:49 +0530

    sidebar_updates

--- src/components/screens/Index.tsx
+++ src/components/screens/Index.tsx
@@ -23,60 +23,88 @@ function Index() {
   };
   return (
     <>
-      <Head title="Home" description="Welcome" />/
-      <div className="carousel w-full">
-        <div id="slide1" className="carousel-item relative w-full">
-          <img src="https://placeimg.com/800/200/arch" className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide4" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide2" className="btn btn-circle">
-              ❯
-            </a>
+      <Head title="Home" description="Welcome" />
+      <div className="flex">
+        <div className="carousel w-full flex-1">
+          <div id="slide1" className="carousel-item relative w-full">
+            <img src="https://placeimg.com/800/200/arch" className="w-full" />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide4" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide2" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
+          </div>
+          <div id="slide2" className="carousel-item relative w-full">
+            <img src="https://placeimg.com/800/200/arch" className="w-full" />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide1" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide3" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
+          </div>
+          <div id="slide3" className="carousel-item relative w-full">
+            <img src="https://placeimg.com/800/200/arch" className="w-full" />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide2" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide4" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
+          </div>
+          <div id="slide4" className="carousel-item relative w-full">
+            <img src="https://placeimg.com/800/200/arch" className="w-full" />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide3" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide1" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
           </div>
         </div>
-        <div id="slide2" className="carousel-item relative w-full">
-          <img src="https://placeimg.com/800/200/arch" className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide1" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide3" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
+        <div>
+          <ul className="menu p-4 w-80 bg-base-100 text-base-content bg-sky-200/100  ">
+            {/* <!-- Sidebar content here --> */}
+            <li>
+              <a>Pocket friendly tourism</a>
+            </li>
+            <li>
+              <a>Kids Attraction</a>
+            </li>
+            <li>
+              <a>Kurukshetra@360</a>
+            </li>
+            <li>
+              <a>Picnic spots</a>
+            </li>
+            <li>
+              <a>Sidebar Item 1</a>
+            </li>
+            <li>
+              <a>Sidebar Item 2</a>
+            </li>
+          </ul>
         </div>
-        <div id="slide3" className="carousel-item relative w-full">
-          <img src="https://placeimg.com/800/200/arch" className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide2" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide4" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
-        <div id="slide4" className="carousel-item relative w-full">
-          <img src="https://placeimg.com/800/200/arch" className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide3" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide1" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
+
+        {/* mmk */}
       </div>
+
       <div className="card card-side bg-base-100 shadow-xl m-10">
         <figure>
           <img width="200px" src="https://placeimg.com/200/280/arch" alt="Movie" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">New movie is released!</h2>
-          <p>Click the button to watch on Jetflix app.</p>
+          <h2 className="card-title">Why visit KKR</h2>
+          <p>Click the button to watch on</p>
           <div className="card-actions justify-end">
             <button className="btn btn-primary">Watch</button>
           </div>
